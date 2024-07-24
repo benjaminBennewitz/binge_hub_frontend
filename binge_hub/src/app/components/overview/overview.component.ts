26c321fdcb2996200bb116ae32e6101fc14e4dd0
Werbung06 <Werbung06@skuvgele.local>
2024-07-24 17:32:00 +0200

    set up video fullscreen

--- binge_hub/src/app/components/overview/overview.component.ts
+++ binge_hub/src/app/components/overview/overview.component.ts
@@ -5,17 +5,33 @@ import { TooltipPosition } from '@angular/material/tooltip';
 @Component({
   selector: 'app-overview',
   templateUrl: './overview.component.html',
-  styleUrl: './overview.component.scss'
+  styleUrls: ['./overview.component.scss'],
 })
 export class OverviewComponent {
-  constructor(
-  ) {}
+  positionOptions: TooltipPosition[] = ['below', 'above', 'left', 'right'];
+  position = new FormControl(this.positionOptions[0]);
 
-   // tooltip positions
-   positionOptions: TooltipPosition[] = ['below', 'above', 'left', 'right'];
-   position = new FormControl(this.positionOptions[0]);
+  constructor() {}
 
-  ngOnInit(): void {
-    
+  ngOnInit(): void {}
+
+  
+ /**
+   * Plays the video in fullscreen mode and hides the control elements after a short time.
+   * - Resets the video to the beginning.
+   */
+  playVideo(): void {
+    const videoElement = document.getElementById('background-video') as HTMLVideoElement;
+    videoElement.currentTime = 0;
+
+    if (videoElement.requestFullscreen) {
+      videoElement.requestFullscreen();
+      videoElement.play();
+
+      //hide video controls after 2 seconds
+      setTimeout(() => {
+        videoElement.controls = false;
+      }, 2000);
+    }
   }
 }
